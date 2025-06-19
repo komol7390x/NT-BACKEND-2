5d938089089f5c54ce79a770b6278f1a45a80c94
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-19 12:51:43 +0500

    up

---
+++ qoshimcha/script.js
@@ -0,0 +1,13 @@
+const fs=require('fs');
+
+fs.readFile('text.txt','utf-8',(err,data)=>{
+    if(err){
+        console.log(err);
+        return
+    }
+    // console.log('BU data: ',data);
+})
+const data=fs.promises.readFile('text.txt','utf-8')
+console.log(data);
+
+data.then(res=>console.log(res)).catch(console.log)
