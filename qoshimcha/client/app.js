c3e75f150c60c6cd349552290e745346b0ea668c
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-26 13:00:16 +0500

    up

---
+++ qoshimcha/client/app.js
@@ -0,0 +1,13 @@
+const { error } = require("console");
+
+const socket=new WebSocket('ws://localhost:3002');
+socket.onopen=function(){
+    console.log('Serverga ulandi');
+    socket.send("Salom server");
+}
+socket.onmessage=(event)=>{
+    console.log(event.data);
+}
+socket.onerror=(error)=>{
+    console.log(error)
+}
--- qoshimcha/server/server.js
+++ qoshimcha/server/server.js
@@ -2,3 +2,21 @@ const WebSocket=require('ws');
 const wss=new WebSocket.Server({port:3002,},()=>{
     console.log('Server is runing 3000');
 })
+
+wss.on('connection',(ws,req)=>{
+    console.log('Client Ulandi');
+
+    ws.on('message',(data)=>{
+        console.log(data.toString());
+    })
+
+    ws.on('error',(err)=>{
+        console.log(err);
+    })
+
+    ws.on('close',()=>{
+        console.log('Client serverdan uzuldi!');
+    });
+
+    ws.send('salom Client');
+})
