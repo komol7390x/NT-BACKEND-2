a485ae0c523e47270d6d00b266dcd29c2693b744
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-06-26 13:04:50 +0500

    up

--- qoshimcha/client/app.js
+++ qoshimcha/client/app.js
@@ -1,5 +1,3 @@
-const { error } = require("console");
-
 const socket=new WebSocket('ws://localhost:3002');
 socket.onopen=function(){
     console.log('Serverga ulandi');
@@ -9,5 +7,5 @@ socket.onmessage=(event)=>{
     console.log(event.data);
 }
 socket.onerror=(error)=>{
-    console.log(error);
+    console.log(error.type);
 }
