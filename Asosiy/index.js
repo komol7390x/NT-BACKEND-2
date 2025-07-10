8a29a79bc81634d79143e75b4f038cf53715d40c
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-10 15:20:00 +0500

    up

---
+++ Asosiy/index.js
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt'
+const resultBcrypt=async(item)=>{
+    const res=await bcrypt.genSalt(3);
+    const hash=await bcrypt.hash(item,res)
+    return hash
+}
+
+const res=resultBcrypt('salom')
+const check=await bcrypt.compare('salom',res);
+console.log(check);
+
